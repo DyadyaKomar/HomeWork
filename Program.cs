f3ae56efcd755c628fc3c8aaceb7c1ef4c556117
unknown <dyadya.komar.02@mail.ru>
2023-06-11 10:44:58 +0300

    СЕМ1

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// System.Console.WriteLine("Введите число квадрат которого вы хотите увидеть ->");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Квадрат числа {num} -> {num*num}");
+
+
+// Task2.
+// 5 25 - ?
+// 25 5 - ?
+
+// System.Console.WriteLine("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+
+
+
+// if(num1 == quad)
+// {
+//     System.Console.WriteLine($"Первое чило {num1} является квадратом второго числа {num2}");
+// }
+// else
+// {
+//   System.Console.WriteLine($"Первое чило {num1} не является квадратом второго числа {num2}");
+// }
+
+
+//Task3
+// Написать программу которая принимает число N и выводит последовательность чисел от - N до N
+
+// System.Console.WriteLine("Введите число N -> ");
+// int n = Convert.ToInt32(Console.ReadLine() );
+
+// int current = n* (-1);
+
+// while (current <= n)
+// {
+// System.Console.Write(current + " ");
+// current ++;
+// }
+
+
+//Task4
+
+// Дается 3х значное число. Нам нужно вывести последнею цифру этого числа
+
+// System.Console.WriteLine("Введите трехзначное  число");
+// int n = Convert.ToInt32(Console.ReadLine() );
+
+// if ( n >= 100 && n <= 999 )
+
+// {
+//    System.Console.WriteLine($"Последняя цифра числа {n} -> {n % 10} ");
+// }
+// else
+// {
+// System.Console.WriteLine("Вы ввели не трехзначное число!");
+// } 
+
+
+
+//HomeWork
+
+// Quests 
+// Дается два числа и надо понять , какое из чисел больше
+System.Console.WriteLine("Введите числа");
+int n = Convert.ToInt32(Console.ReadLine() );
